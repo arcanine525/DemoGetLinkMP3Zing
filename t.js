08d4271a8041e81607113477e68a3e76d719a75b
Hiep Le Hoang <Hiep Le Hoang>
2017-08-22 15:47:29 +0700

    Faster!!

--- t.js
+++ t.js
@@ -1,8 +1,9 @@
 var request = require('request');
+var oriLink = 'http://mp3.zing.vn/bai-hat/Xin-Dung-Lang-Im-Soobin-Hoang-Son/ZW80B6I8.html'
 var MyHeaders = {
     'X-Requested-With': 'XMLHttpRequest',
     'Connection': 'keep-alive',
-    'Cookie': 'tuser=1; __mp3sessid=A828B942D1C4; _znu=1; fuid=de9aa29d9873988881b2cecce97bf3f4; __gads=ID=c42680b7198b7732:T=1495113971:S=ALNI_MYMC7VV6X6ZIan7dA2hEMIrhYt1xw; SRVID=s65178_8131; zpw_sek=null; wzvid=2000.a6307024dc5834066d49.1495721046572.0021bfc8; zsid=null; wsid=R5m6.101001015.3.jikudNlM9e1CyIE6UzOcWD0kTbW9v-nVUavYwYBM9e0; 360GAME_ACCESS=true; __zlcmid=gygVsvadl3AguB; zmsid=Lcrc.54913949.10.yg8qzDxtWxr37CQ-rVU3lR-5uDomdktOwyMsoQYNTC_lFLoyjhC7v0; __utma=1.1928836389.1495040002.1497189416.1497189416.1; __utmc=1; __utmz=1.1497189416.1.1.utmcsr=mp3.zing.vn|utmccn=(referral)|utmcmd=referral|utmcct=/vip; _zploc=A1953915124; ___zlid=403.2578798297.1953915124.1502528714.1436031958; _zmp3=0.17363880783041674; _gat=1; _ga=GA1.2.1928836389.1495040002; _gid=GA1.2.409523974.1502338811; ___sessid=2372.2578325868.3456446593.1502529097.616434284; __sessid=5093.2578328589.3456449314.1502529097; BANNER_OFF=1; atmpv=9; __zi=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; adtimaUserId=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; crtg_vng_rta=bmi300250H%3D1%3Bmobmi300250%3D1%3Bm3z300600%3D1%3Blbn300600%3D1%3Bnwz1190250%3D1%3Bnwz360640%3D1%3Bbmi360640%3D1%3Bm3z1190250%3D1%3Blbn970250%3D1%3B'
+    'Cookie': 'tuser=1; _znu=1; fuid=de9aa29d9873988881b2cecce97bf3f4; __gads=ID=c42680b7198b7732:T=1495113971:S=ALNI_MYMC7VV6X6ZIan7dA2hEMIrhYt1xw; zpw_sek=null; wzvid=2000.a6307024dc5834066d49.1495721046572.0021bfc8; zsid=null; wsid=R5m6.101001015.3.jikudNlM9e1CyIE6UzOcWD0kTbW9v-nVUavYwYBM9e0; __zlcmid=gygVsvadl3AguB; __utma=1.1928836389.1495040002.1497189416.1497189416.1; __utmz=1.1497189416.1.1.utmcsr=mp3.zing.vn|utmccn=(referral)|utmcmd=referral|utmcct=/vip; SRVID=s65174_8133; __mp3sessid=DFEC7D827FA5; zmsid=Sqmv.54913949.13.J7d0q4qV45NGz5LMHXyGLInjSpGZTdumU2qb_hUy0MnGEB9K1Mo930; _zploc=A3419216615; _zmp3=0.7899024572239537; BANNER_OFF=1; _ga=GA1.2.1928836389.1495040002; _gid=GA1.2.689069859.1503336636; atmpv=11; __zi=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; adtimaUserId=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; crtg_vng_rta=m3z3002501%3D1%3Bm3z3002502%3D1%3Bbmi72890%3D1%3Bbmi300250H%3D1%3Bbmi300250S%3D1%3Bnwz300600%3D1%3Bzoom300600%3D1%3Bmom3z300250%3D1%3Bmobmi32050%3D1%3Bmonwz300250%3D1%3Bmobmi300250%3D1%3Bnwzint300600%3D1%3Bnwz300600m%3D1%3Bztv97090%3D1%3Bztvcb97090%3D1%3Bm3z300600%3D1%3Bm3zah300250%3D1%3Bm3zap300250%3D1%3Bztv970250%3D1%3Blbn300250%3D1%3Blbn72890%3D1%3Bbmi160600%3D1%3Bbmi300600%3D1%3Blbn300600%3D1%3Blbn97090%3D1%3Blbn200200%3D1%3Bnwz1190250%3D1%3Bnwz360640%3D1%3Bbmi360640%3D1%3Bm3z1190250%3D1%3Bbmi1p300250%3D1%3Blbn160600%3D1%3Blbn970250%3D1%3B; __sessid=231.1503391400.3409791234.1503391169; ___zlid=55.2579150211.3419216615.1503391169.2272224960; ___sessid=73.2579150229.1906399834.1503391169.510652287'
 };
 
 function getDowloadLink(options) {
@@ -125,4 +126,61 @@ async function sysn(oriLink, headers) {
     console.log(arrLink);
 }
 
-sysn("http://mp3.zing.vn/bai-hat/Lac-Nhau-Co-Phai-Muon-Doi-ERIK-ST319/ZW78D0FZ.html", MyHeaders);
+//sysn("http://mp3.zing.vn/bai-hat/Lac-Nhau-Co-Phai-Muon-Doi-ERIK-ST319/ZW78D0FZ.html", MyHeaders);
+
+
+
+var options = {
+    url: oriLink,
+    headers: MyHeaders,
+    gzip: 'true'
+};
+
+
+async function getMp3Info(options) {
+    let linkXml = await getXmlLink(options);
+    let info = await getInfo(o = {
+        url: linkXml,
+        headers: MyHeaders,
+        gzip: 'true'
+    })
+    return Promise.resolve(info)
+
+}
+
+
+async function getMp3DownloadLink(options, headers) {
+    let linkDownload = await getDowloadLink(options);
+    let sourceLink = await getSourceLink(o = {
+        url: linkDownload,
+        headers: headers,
+        gzip: 'true'
+    })
+    //console.log(sourceLink)
+    //return Promise.resolve(sourceLink)
+    let arrLink = await Promise.all([getDirectLink(o = {
+        url: 'http://mp3.zing.vn' + sourceLink['128']['link'],
+        headers: headers,
+        gzip: 'true'
+    }),
+    getDirectLink(o = {
+        url: 'http://mp3.zing.vn' + sourceLink['320']['link'],
+        headers: headers,
+        gzip: 'true'
+    }),
+    getDirectLink(o = {
+        url: 'http://mp3.zing.vn' + sourceLink['lossless']['link'],
+        headers: headers,
+        gzip: 'true'
+    })])
+    return Promise.resolve(arrLink);
+}
+
+async function sysnAll(options, headers) {
+    let info = await getMp3Info(options);
+    let arrLink = await getMp3DownloadLink(options, headers);
+    console.log(info['name'])
+    console.log("LINK: " + arrLink[2])
+
+}
+sysnAll(options, MyHeaders);
--- test.js
+++ test.js
@@ -3,19 +3,18 @@ var request = require('request');
 var headers = {
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'vi,en-US;q=0.8,en;q=0.6',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
     'Accept': '*/*',
+    'Referer': 'http://mp3.zing.vn/bai-hat/Minh-La-Gi-Cua-Nhau-Lou-Hoang/ZW78I80B.html',
     'X-Requested-With': 'XMLHttpRequest',
     'Connection': 'keep-alive',
-    'Cookie': 'tuser=1; __mp3sessid=A828B942D1C4; _znu=1; fuid=de9aa29d9873988881b2cecce97bf3f4; __gads=ID=c42680b7198b7732:T=1495113971:S=ALNI_MYMC7VV6X6ZIan7dA2hEMIrhYt1xw; SRVID=s65178_8131; zpw_sek=null; wzvid=2000.a6307024dc5834066d49.1495721046572.0021bfc8; zsid=null; wsid=R5m6.101001015.3.jikudNlM9e1CyIE6UzOcWD0kTbW9v-nVUavYwYBM9e0; 360GAME_ACCESS=true; __zlcmid=gygVsvadl3AguB; zmsid=Lcrc.54913949.10.yg8qzDxtWxr37CQ-rVU3lR-5uDomdktOwyMsoQYNTC_lFLoyjhC7v0; __utma=1.1928836389.1495040002.1497189416.1497189416.1; __utmc=1; __utmz=1.1497189416.1.1.utmcsr=mp3.zing.vn|utmccn=(referral)|utmcmd=referral|utmcct=/vip; _zploc=A1953915124; ___zlid=403.2578798297.1953915124.1502528714.1436031958; _zmp3=0.17363880783041674; _gat=1; _ga=GA1.2.1928836389.1495040002; _gid=GA1.2.409523974.1502338811; ___sessid=2372.2578325868.3456446593.1502529097.616434284; __sessid=5093.2578328589.3456449314.1502529097; BANNER_OFF=1; atmpv=9; __zi=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; adtimaUserId=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; crtg_vng_rta=bmi300250H%3D1%3Bmobmi300250%3D1%3Bm3z300600%3D1%3Blbn300600%3D1%3Bnwz1190250%3D1%3Bnwz360640%3D1%3Bbmi360640%3D1%3Bm3z1190250%3D1%3Blbn970250%3D1%3B'
+    'Cookie': 'tuser=1; _znu=1; fuid=de9aa29d9873988881b2cecce97bf3f4; __gads=ID=c42680b7198b7732:T=1495113971:S=ALNI_MYMC7VV6X6ZIan7dA2hEMIrhYt1xw; zpw_sek=null; wzvid=2000.a6307024dc5834066d49.1495721046572.0021bfc8; zsid=null; wsid=R5m6.101001015.3.jikudNlM9e1CyIE6UzOcWD0kTbW9v-nVUavYwYBM9e0; __zlcmid=gygVsvadl3AguB; __utma=1.1928836389.1495040002.1497189416.1497189416.1; __utmz=1.1497189416.1.1.utmcsr=mp3.zing.vn|utmccn=(referral)|utmcmd=referral|utmcct=/vip; SRVID=s65174_8133; __mp3sessid=DFEC7D827FA5; zmsid=Sqmv.54913949.13.J7d0q4qV45NGz5LMHXyGLInjSpGZTdumU2qb_hUy0MnGEB9K1Mo930; _zploc=A3419216615; _zmp3=0.7899024572239537; BANNER_OFF=1; _ga=GA1.2.1928836389.1495040002; _gid=GA1.2.689069859.1503336636; atmpv=11; __zi=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; adtimaUserId=2000.fed4c60ef57e1d20446f.1495040031090.e6e9c767; crtg_vng_rta=m3z3002501%3D1%3Bm3z3002502%3D1%3Bbmi72890%3D1%3Bbmi300250H%3D1%3Bbmi300250S%3D1%3Bnwz300600%3D1%3Bzoom300600%3D1%3Bmom3z300250%3D1%3Bmobmi32050%3D1%3Bmonwz300250%3D1%3Bmobmi300250%3D1%3Bnwzint300600%3D1%3Bnwz300600m%3D1%3Bztv97090%3D1%3Bztvcb97090%3D1%3Bm3z300600%3D1%3Bm3zah300250%3D1%3Bm3zap300250%3D1%3Bztv970250%3D1%3Blbn300250%3D1%3Blbn72890%3D1%3Bbmi160600%3D1%3Bbmi300600%3D1%3Blbn300600%3D1%3Blbn97090%3D1%3Blbn200200%3D1%3Bnwz1190250%3D1%3Bnwz360640%3D1%3Bbmi360640%3D1%3Bm3z1190250%3D1%3Bbmi1p300250%3D1%3Blbn160600%3D1%3Blbn970250%3D1%3B; __sessid=231.1503391400.3409791234.1503391169; ___zlid=55.2579150211.3419216615.1503391169.2272224960; ___sessid=73.2579150229.1906399834.1503391169.510652287'
 };
 
 var options = {
-    url: 'http://mp3.zing.vn/download/song/Anh-Se-Tot-Ma-Pham-Hong-Phuoc-Thuy-Chi-Pham-Hong-Phuoc-Thuy-Chi/knJmtLGspzdJXcByVFGybmLGTkQHbxngsms?sig=c937d0acbc55b652b14061469d98f44b',
+    url: 'http://mp3.zing.vn/json/song/get-download?panel=.fn-tab-panel-service&code=kncnykGaWsQcuJsTkbJTFHZG&group=.fn-tab-panel',
     headers: headers,
-    gzip: 'true',
-    allow_redirects: 'false',
-    followRedirect: 'true'
+    gzip: 'true'
 };
 
 function callback(error, response, body) {
@@ -24,4 +23,4 @@ function callback(error, response, body) {
     }
 }
 
-request(options, callback)
+request(options, callback);
